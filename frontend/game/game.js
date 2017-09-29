a1d7e623d661505dab8826b637efdf8b3a193ba4
simon <dakrulski@gmail.com>
2017-09-29 18:32:16 +0200

    Add agents to game

--- frontend/game/game.js
+++ frontend/game/game.js
@@ -3,6 +3,7 @@ import * as SAT from 'sat';
 import Viewport from './system/viewport.js';
 import Textures from './system/textures.js';
 import Suit from './objects/suit.js';
+import Agent from './objects/agent.js';
 import { wanderer_random } from './controller/wanderer.js';
 import { random_int_from_interval } from './system/functions.js';
 
@@ -12,7 +13,7 @@ class Game extends PIXI.Application {
 
         this.texture = new Textures();
 
-        //this.loader.add('suit_png', '/static/models/suit/suit.png', {'metadata': {'type': 'spritesheet'}});
+        // SUIT
         this.loader.add('suit_idle1', '/static/models/suit/idle1.png', {
             'metadata': {
                 'type': 'multi_spritesheet',
@@ -34,7 +35,57 @@ class Game extends PIXI.Application {
                 'action_name': 'walk'
             }
         });
+        this.loader.add('suit_run', '/static/models/suit/run.png', {
+            'metadata': {
+                'type': 'multi_spritesheet',
+                'group_name': 'suit',
+                'action_name': 'run'
+            }
+        });
         this.loader.add('suit', '/static/models/suit/suit.json', {'metadata': {'type': 'multi_anim', 'group_name': 'suit'}});
+
+        // AGENT
+        this.loader.add('agent_idle1', '/static/models/agent/idle1.png', {
+            'metadata': {
+                'type': 'multi_spritesheet',
+                'group_name': 'agent',
+                'action_name': 'idle1'
+            }
+        });
+
+        this.loader.add('agent_idle2', '/static/models/agent/idle2.png', {
+            'metadata': {
+                'type': 'multi_spritesheet',
+                'group_name': 'agent',
+                'action_name': 'idle2'
+            }
+        });
+
+        this.loader.add('agent_walk', '/static/models/agent/walk.png', {
+            'metadata': {
+                'type': 'multi_spritesheet',
+                'group_name': 'agent',
+                'action_name': 'walk'
+            }
+        });
+
+        this.loader.add('agent_run', '/static/models/agent/run.png', {
+            'metadata': {
+                'type': 'multi_spritesheet',
+                'group_name': 'agent',
+                'action_name': 'run'
+            }
+        });
+
+        this.loader.add('agent_shot1', '/static/models/agent/shot1.png', {
+            'metadata': {
+                'type': 'multi_spritesheet',
+                'group_name': 'agent',
+                'action_name': 'shot1'
+            }
+        });
+        this.loader.add('agent', '/static/models/agent/agent.json', {'metadata': {'type': 'multi_anim', 'group_name': 'agent'}});
+
 
         this.loader.load((loader, resources) => {
 
@@ -44,11 +95,12 @@ class Game extends PIXI.Application {
 
             this.texture.create_textures(resources);
 
-            for (let suit_cnt = 0; suit_cnt <= 80; suit_cnt++) {
+            for (let suit_cnt = 0; suit_cnt <= 40; suit_cnt++) {
 
                 var suit_anim_dict = this.texture.anim.suit;
                 //todo: the animation speeds should be added to the texture-object right after it is created
                 suit_anim_dict['_animation_speeds'] = {
+                    'run': 1.5,
                     'walk': 1,
                     'idle': 0.2,
                     'idle2': 0.1
@@ -57,7 +109,25 @@ class Game extends PIXI.Application {
                 suit.install_controller(wanderer_random, {'min_x': 50, 'min_y': 50, 'max_x': 750, 'max_y': 550});
 
                 this.viewport.add_object(suit);
+                console.log('HERE A PERSON');
+                console.log(suit);
+            }
+            
+            for (let agent_cnt = 0; agent_cnt <= 10; agent_cnt++) {
 
+                var agent_anim_dict = this.texture.anim.agent;
+                agent_anim_dict['_animation_speeds'] = {
+                    'run': 1.5,
+                    'walk': 1,
+                    'idle': 0.2,
+                    'idle2': 0.1
+                };
+                var agent = new Agent(random_int_from_interval(80,700), random_int_from_interval(80,500), agent_anim_dict);
+                agent.install_controller(wanderer_random, {'min_x': 50, 'min_y': 50, 'max_x': 750, 'max_y': 550});
+
+                this.viewport.add_object(agent);
+                console.log('HERE A AGENT');
+                console.log(agent);
             }
 
             this.ticker.add(function() {
